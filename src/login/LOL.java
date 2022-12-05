c839d4e9f4187795bdb74e50fd502dc6f0c7813c
Mateo De León <mateodeleon903@gmail.com>
2022-12-05 16:02:05 -0300

    Poronga

--- src/login/LOL.java
+++ src/login/LOL.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
 
 
@@ -28,19 +29,21 @@ public class LOL extends JFrame{
         // Initialize components
 
         keepSigned = new JCheckBox();
-        keepSigned.setBounds(0, 0, 0, 0);
+        keepSigned.setBounds(80, 347, 114, 21);
         showPassword = new JCheckBox();
-        showPassword.setBounds(0, 0, 0, 0);
+        showPassword.setBounds(287, 296, 91, 21);
 
         userField = new JTextField();
-        userField.setBounds(0, 0, 0, 0);
+        userField.setBounds(80, 213, 201, 20);
         userLabel = new JLabel("User"); 
-        userLabel.setBounds(0, 0, 0, 0);userLabel.setLabelFor(userField);
+        userLabel.setLocation(80, 188);
+        userLabel.setSize(58, 14);
 
         passwordField = new JPasswordField();
-        passwordField.setBounds(0, 0, 0, 0);
+        passwordField.setBounds(80, 297, 201, 20);
         passwordLabel = new JLabel("Password"); 
-        passwordLabel.setBounds(0, 0, 384, 561);passwordLabel.setLabelFor(passwordField);
+        passwordLabel.setLocation(80, 272);
+        passwordLabel.setSize(58, 14);
 
 
         setSize(400, 600);
@@ -55,6 +58,10 @@ public class LOL extends JFrame{
 
         getContentPane().add(passwordField);
         getContentPane().add(passwordLabel);
+        
+        JButton submit = new JButton("Submit");
+        submit.setBounds(80, 407, 201, 47);
+        getContentPane().add(submit);
 
 
 
